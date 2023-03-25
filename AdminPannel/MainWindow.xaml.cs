1c9210d41c431233fa7a5953e1151d0fbecaa243
PasanAbeysekara <69195287+PasanAbeysekara@users.noreply.github.com>
2023-03-25 22:00:32 +0530

    Add project files.

---
+++ AdminPannel/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace AdminPannel
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Border_MouseDown(object sender,MouseButtonEventArgs e)
+		{
+			if(e.ChangedButton == MouseButton.Left)
+			{
+				this.DragMove();
+			}
+		}
+
+	}
+}
